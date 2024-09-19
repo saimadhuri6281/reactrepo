9675d3b3e9ccc20270ec61b4370baf1b02f8e4cd
saimadhuri6281 <saimadhuri6281@gmail.com>
2024-09-19 10:45:44 +0530

    Update Products1.jsx

--- src/Data/Products1.jsx
+++ src/Data/Products1.jsx
@@ -1,24 +1,108 @@
-import table1 from '../Assets/table1.jpg';
-import table2 from '../Assets/table1.jpg';
-import table3 from '../Assets/table1.jpg';
+import sports1 from '../Assets/sports1.jpg';
+import sports2 from '../Assets/sports2.jpg';
+import sports3 from '../Assets/sports3.jpg';
+import sports4 from '../Assets/sports4.jpg';
+import sports5 from '../Assets/sports5.jpg';
+import sports6 from '../Assets/sports6.jpg';
+import sports7 from '../Assets/sports7.jpg';
+import sports8 from '../Assets/sports8.jpg';
+import sports9 from '../Assets/sports9.jpg';
+import sports10 from '../Assets/sports10.jpg';
+import sports11 from '../Assets/sports11.jpg';
+import sports12 from '../Assets/sports12.jpg';
+import sports13 from '../Assets/sports13.jpg';
+import sports14 from '../Assets/sports14.jpg';
+import sports15 from '../Assets/sports15.jpg';
 
 export const productsData = [
     {
         "id": 1,
-        "image": table1,
+        "image": sports1,
         "ProductName": "",
-        "price": 20000,
+        "price": 2000,
     },
     {
         "id": 2,
-        "image": table2,
+        "image": sports2,
         "ProductName": "",
-        "price": 17000,
+        "price": 1700,
     },
     {
         "id": 3,
-        "image": table3,
+        "image": sports3,
         "ProductName": "",
-        "price": 22000,
+        "price": 2200,
+    },
+    {
+        "id": 4,
+        "image": sports4,
+        "ProductName": "",
+        "price": 2200,
+    },
+    {
+        "id": 5,
+        "image": sports5,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 6,
+        "image": sports6,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 7,
+        "image": sports7,
+        "ProductName": "",
+        "price": 1900,
+    },
+    {
+        "id": 8,
+        "image": sports8,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 9,
+        "image": sports9,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 10,
+        "image": sports10,
+        "ProductName": "",
+        "price": 3900,
+    },
+    {
+        "id": 11,
+        "image": sports11,
+        "ProductName": "",
+        "price": 2500,
+    },
+    {
+        "id": 12,
+        "image": sports12,
+        "ProductName": "",
+        "price": 2400,
+    },
+    {
+        "id": 13,
+        "image": sports13,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 14,
+        "image": sports14,
+        "ProductName": "",
+        "price": 1800,
+    },
+    {
+        "id": 15,
+        "image": sports15,
+        "ProductName": "",
+        "price": 2900,
     },
 ]
