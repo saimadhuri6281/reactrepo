57cf759eb46559cdbf81a9105ad7809c4caaedfa
saimadhuri6281 <saimadhuri6281@gmail.com>
2024-09-19 10:56:52 +0530

    Update Products2.jsx

--- src/Data/Products2.jsx
+++ src/Data/Products2.jsx
@@ -1,1 +1,108 @@
+import game1 from '../Assets/game1.jpg';
+import game2 from '../Assets/game2.jpg';
+import game3 from '../Assets/game3.jpg';
+import game4 from '../Assets/game4.jpg';
+import game5 from '../Assets/game5.jpg';
+import game6 from '../Assets/game6.jpg';
+import game7 from '../Assets/game7.jpg';
+import game8 from '../Assets/game8.jpg';
+import game9 from '../Assets/game9.jpg';
+import game10 from '../Assets/game10.jpg';
+import game11 from '../Assets/game11.jpg';
+import game12 from '../Assets/game12.jpg';
+import game13 from '../Assets/game13.jpg';
+import game14 from '../Assets/game14.jpg';
+import game15 from '../Assets/game15.jpg';
+export const productsData = [
+    {
+        "id": 1,
+        "image": game1,
+        "ProductName": "",
+        "price": 2000,
+    },
+    {
+        "id": 2,
+        "image": game2,
+        "ProductName": "",
+        "price": 1700,
+    },
+    {
+        "id": 3,
+        "image": game3,
+        "ProductName": "",
+        "price": 2200,
+    },
+    {
+        "id": 4,
+        "image": game4,
+        "ProductName": "",
+        "price": 2200,
+    },
+    {
+        "id": 5,
+        "image": game5,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 6,
+        "image": game6,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 7,
+        "image": game7,
+        "ProductName": "",
+        "price": 1900,
+    },
+    {
+        "id": 8,
+        "image": game8,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 9,
+        "image": game9,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 10,
+        "image": game10,
+        "ProductName": "",
+        "price": 3900,
+    },
+    {
+        "id": 11,
+        "image": game11,
+        "ProductName": "",
+        "price": 2500,
+    },
+    {
+        "id": 12,
+        "image": game12,
+        "ProductName": "",
+        "price": 2400,
+    },
+    {
+        "id": 13,
+        "image": game13,
+        "ProductName": "",
+        "price": 2900,
+    },
+    {
+        "id": 14,
+        "image": game14,
+        "ProductName": "",
+        "price": 1800,
+    },
+    {
+        "id": 15,
+        "image": game15,
+        "ProductName": "",
+        "price": 2900,
+    },
+]
 
