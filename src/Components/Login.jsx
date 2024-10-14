b0e88fa1a48e3d1e8c6dbd90fde46c7ad813054c
saimadhuri6281 <saimadhuri6281@gmail.com>
2024-10-14 09:29:54 +0530

    Update and rename login.jsx to Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 function Login() {
   return (
-    <div className='container-fluid loginbody'>
+    <div className='loginbody'>
       <div className='main'>
+        <h1 className='heading'>Sportz</h1>
         <div className='block'>
-         
           <h4>UserName</h4>
-          <input type="text" placeholder='Enter your User Name' /><br /><br />
+          <input type="text" className="input-field" placeholder='Enter your User Name' /><br />
           <h4>Password</h4>
-          <input type="password" placeholder='Enter your Password' />
+          <input type="password" className="input-field" placeholder='Enter your Password' />
         </div>
         <div className='signup'>
           <Link to='/dashboard'>
-            <button>Log in</button>
+            <button className='login-button'>Log in</button>
           </Link>
           <div className='skip-option'>
-            <Link to='/dashboard'>
+            <Link to='/'>
               <h3>Skip</h3>
             </Link>
           </div>         
