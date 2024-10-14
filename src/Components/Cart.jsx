0d019cc6cf40a961c9eb40249b538c2b8c773ad6
saimadhuri6281 <saimadhuri6281@gmail.com>
2024-10-14 09:47:54 +0530

    Update Cart.jsx

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,1 +1,24 @@
+// Cart.js
+import React from 'react';
+
+const Cart = ({ cartItems }) => {
+  return (
+    <div className="cart">
+      <h3>Your Cart:</h3>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cartItems.map((item, index) => (
+            <li key={index}>
+              {item.name} - ${item.price}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
 
